f138dfe03552697f9830202c9cd395d819eccfd7
alekubar <alejokubar@gmail.com>
2024-12-31 14:26:04 -0300

    Delete usuario directory

--- usuario/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def login(request):
-    return render(request, 'usuario/login.html')
-
-def perfil(request):
-    return render(request, 'usuario/perfil.html')
-
