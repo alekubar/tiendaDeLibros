9d4adddcafe3541d1c1906ac5c22d62a6ca5bce9
alekubar <alejokubar@gmail.com>
2024-12-31 14:24:36 -0300

    Delete src directory

--- src/books/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.conf.urls.contrib import  admin
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include(""))
-]
